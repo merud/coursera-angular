cab8d4c4902907f130672d064929eb49e9197e23
Michael <mrudqu@gmail.com>
2020-10-27 15:12:46 -0500

    pulls menu and compares search value

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -7,10 +7,40 @@
 
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
+        var narrowItDown = this;
 
+        narrowItDown.narrowDown = function () {
+            var promise = MenuSearchService.getMatchedMenuItems(narrowItDown.searchTerm);
+            promise.then(function (response) {
+                narrowItDown.found = response;
+            }).catch(function () {
+                console.log("something went wrong");
+            })
+        }
     }
 
-    function MenuSearchService() {
-        
+    MenuSearchService.$inject = ['$http'];
+    function MenuSearchService($http) {
+        var service = this;
+
+        service.getMatchedMenuItems = function (searchTerm) {
+            return $http({
+                method: "GET",
+                url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+            }).then(function (result) {
+                // process result and only keep items that match
+                var fullList = result.data;
+                var listItems = fullList.menu_items;
+                var foundItems = [];
+
+                listItems.forEach(function (element) {
+                    if (element.description.includes(searchTerm.toLowerCase())) {
+                        foundItems.push(element);
+                    }
+                })
+                // return processed items
+                return foundItems;
+            });
+        }
     }
 })();
